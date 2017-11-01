998eb2a0d0eef58cacbf9d2c70d066b1f8f034c3
pbamotra <pbamotra@coupang.com>
2017-10-31 17:56:13 -0700

    Published: Tue Oct 31 17:56:12 PDT 2017

--- publish.sh
+++ publish.sh
@@ -5,6 +5,7 @@ cd ~/Downloads/ynorm/
 git add .
 git commit -m "Published: $now"
 git push origin master
+curl -X POST -d '' https://api.netlify.com/build_hooks/59f91b880b79b70963232f17
 cd ~/Downloads/pbamotra.github.io/
 git add .
 git commit -m "Published: $now"
